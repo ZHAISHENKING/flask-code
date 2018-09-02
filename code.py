882a63f8ba09d698e11e96484840449f214a9b29
ZHAISHENKING <18700790825@163.com>
2018-09-02 23:16:24 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,34 +1,24 @@
-from flask import request, session, jsonify, current_app
-from flask_restful import Resource
-from util.common import *
-from admin import r
 import gvcode
 
-class Code(Resource):
+def code():
     """
     图形验证码
     """
-    @catch_exception
-    def get(self):
-        base64_str, code = gvcode.base64()
-
-        # 把code存到session中
-        session['verify_code'] = code
-        # 把base64_str 返回给用户
-        return str(base64_str, "utf-8")
+    base64_str, code = gvcode.base64()
+    # 把code存到session中
+    session['verify_code'] = code
+    # 把base64_str 返回给用户
+    return str(base64_str, "utf-8")
 
 
-class VerifyCode(Resource):
+def verify_code(code):
     """
     验证code
     """
-    @catch_exception
-    def post(self):
-        data = request.get_json()
-        user_code = data['code'].lower()
-        # 获取实际的验证码
-        act_code = session.get('verify_code').lower()
-        if user_code == act_code:
-            return trueReturn("验证成功")
-        else:
-            return falseReturn("验证失败")
+    user_code = code.lower()
+    # 获取实际的验证码
+    act_code = session.get('verify_code').lower()
+    if user_code == act_code:
+        return "验证成功"
+    else:
+        return "验证失败"
